0d318b50479c214f1d1db254b0aae0c81a870f1d
quartz010 <quartz010@outlook.com>
2019-09-14 04:09:13 +0800

    合并feed参数

--- rss.py
+++ rss.py
@@ -1,39 +1,52 @@
-import feedparser
-import ssl
-import json
 
-ssl._create_default_https_context = ssl._create_unverified_context
 
-# d = feedparser.parse('https://blog.12ms.xyz/feed/')
-d = feedparser.parse('https://rsshub.app/dysfz')
+def parse_rss(feed_url):
+    import datetime
+    import json
+    import feedparser
+    import ssl
 
-# print(json.dumps(dict(d)))
-print(d.feed.title)
-print(d.channel.title)
-print(d.feed.link)
-print(d.feed.subtitle)
-print(d.channel.description)
+    ssl._create_default_https_context = ssl._create_unverified_context
+    d = feedparser.parse('https://blog.12ms.xyz/feed/')
+    # d = feedparser.parse('https://rsshub.app/dysfz')
 
-print('items length is %d' % (len(d['entries']),))
 
-try:
-    print(list(map( lambda x: {'title': x.title, 'link': x.link, 'author': x.author, 'description': x.description, 'tags': list(map(lambda y: y['term'] ,x.tags))}, d.entries)))
-except (KeyError,AttributeError) as e:
+
+    # print(json.dumps(dict(d)))
+    print(d.feed.title)
+    print(d.channel.title)
+    print(d.feed.link)
+    print(d.feed.subtitle)
+    print(d.channel.description)
+    feed_info = {
+        "feed_title":  d.feed.title,
+        "feed_link":  d.feed.link,
+        "feed_desc": d.feed.description,
+        "timestamp": datetime.now()
+    }
+    print('items length is %d' % (len(d['entries']),))
+    rst = list()
     try:
-        print(list(map( lambda x: {'title': x.title, 'link': x.link, 'description': x.description}, d.entries)))
-    except (KeyError,AttributeError) as identifier:
-        print(repr(e))
-# for item in d.entries:
-#     print('item title = %s' % (item.title,))
-#     print('item link = %s' % (item.link,))
-#     print('item author = %s' % (item.author,))
-#     print('item summary = %s' % (item.summary,))
+        rst = list(map( lambda x: dict({'title': x.title, 'link': x.link, 'author': x.author, 'description': x.description, 'tags': list(map(lambda y: y['term'] ,x.tags))}, **feed_info), d.entries))
+    except (KeyError,AttributeError) as e:
+        try:
+            rst = list(map( lambda x: dict({'title': x.title, 'link': x.link, 'description': x.description}, **feed_info), d.entries))
+        except (KeyError,AttributeError) as e:
+            print(repr(e))
 
-#     tags = []
-#     for tag in item.tags:
-#         tags.append(tag.term)
-#     print('item\'s tags = %s ' % (','.join(tags),))
+    return rst
+    # for item in d.entries:
+    #     print('item title = %s' % (item.title,))
+    #     print('item link = %s' % (item.link,))
+    #     print('item author = %s' % (item.author,))
+    #     print('item summary = %s' % (item.summary,))
 
-#     print('item\'s updated time = ',item.updated_parsed)
+    #     tags = []
+    #     for tag in item.tags:
+    #         tags.append(tag.term)
+    #     print('item\'s tags = %s ' % (','.join(tags),))
+
+    #     print('item\'s updated time = ',item.updated_parsed)
 
 
+print(parse_rss("sa"))
