846bcce3b5ce527c0d7a74b813c0695bbcdf2d41
quartz010 <quartz010@outlook.com>
2019-09-15 02:25:24 +0800

    函数返回

--- elastic.py
+++ elastic.py
@@ -12,7 +12,6 @@ def _es_chk_exist(title):
 
 def es_index(bodys):
 
-    es = elasticsearch.Elasticsearch(['sgk:9200'])
     bodys = list(filter(lambda x: not  _es_chk_exist(x['title']), bodys))
     print(bodys)
     for body in bodys:
@@ -23,18 +22,19 @@ def es_index(bodys):
 
 def es_search(kword):
     import json
-    es = elasticsearch.Elasticsearch(['sgk:9200'])
     #res = es.search(index="test", body={"query": {"match_all": {}}})
     #res = es.search(index="test", q="author : *")
     res = es.search(index="test", q="author : {kword} or title : {kword} or description : {kword}".format(kword=kword))
     print("Got %d Hits:" % res['hits']['total']['value'])
     #open('es.json', "w+").write(json.dumps(res))
+    res_list = list()
     for hit in res['hits']['hits']:
         try:
             #print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
-            print(hit["_source"], hit["_score"])
+            print(dict(hit["_source"], **{"_score" :hit["_score"]}))
+            res_list.append(dict(hit["_source"], **{"_score" :hit["_score"]}))
         except KeyError as e:
             print(repr(e))
-
+    return res_list
     #rst = es.get(index="test", id=1)
     #print(rst)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import rss
 import elastic 
+import json
 if __name__ == "__main__":
     #items = rss.parse_rss('https://blog.12ms.xyz/feed')
     #items = rss.parse_rss('https://www.centos.bz/feed/')
     #elastic.es_index(items)
-    elastic.es_search("卡夫卡")
+    res_list = elastic.es_search("卡夫卡")
+    open('res.json', "w+").write(json.dumps(res_list))
     #elastic._es_chk_exist(u'Kafka 简单部署使用sdsdsd指北')
