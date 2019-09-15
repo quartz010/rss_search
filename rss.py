736b63b30d8ecdf0b9c83de303c158dfeadcaaf8
ramonesliu <ramonesliu@tencent.com>
2019-09-15 19:23:31 +0800

    作者缺省为unknown

--- rss.py
+++ rss.py
@@ -76,7 +76,11 @@ def parse_rss(feed_url):
         for _ in range(len(code_str.split('  '))):
             try:
                 code_str = rm_not_exist(code_str, err_item)
+                if err_item == "author": 
+                    feed_info['author'] = "unknown"
+                    
                 rst = list(map( lambda x: dict(eval(code_str), **feed_info), d.entries))
+                    
             except AttributeError as e:
                     err_item = re.compile("'(.*)'").findall(repr(e))
                     continue
