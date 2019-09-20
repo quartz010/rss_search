a748033ba58cd186326b6cc5e1fa2c62c596b3cc
ramonesliu <ramonesliu@tencent.com>
2019-09-20 23:58:30 +0800

    调整借口

--- backend/mods/elastic.py
+++ backend/mods/elastic.py
@@ -3,30 +3,30 @@ import elasticsearch
 import datetime
 import time
 from elasticsearch import helpers
+
 es = elasticsearch.Elasticsearch(['sgk:9200'])
 
-def _es_chk_exist(title):
+def _es_chk_exist(_index, _title):
     """
         用来判断是否条目已存在的函数，这里会是一个性能瓶颈，需要进行单条的遍历
     """
-    res = es.search(index="test", q="title : \"{kword}\"".format(kword=title))
+    res = es.search(index=_index, q="title : \"{kword}\"".format(kword=_title))
     return False if res['hits']['total']['value'] == 0 else True
 
-def es_index(bodys):
-
-    bodys = list(filter(lambda x: not  _es_chk_exist(x['title']), bodys))
+def es_index(_index, _bodys):
+    bodys = list(filter(lambda x: not  _es_chk_exist(_index, x['title']), _bodys))
     print('add:' +str(len(bodys)))
     for body in bodys:
         body = dict(body ,**{"timestamp": datetime.datetime.utcnow()})
-        rst = es.index(index='test', body=body, id=None)
+        rst = es.index(index=_index, body=_bodys, id=None)
         print(rst['result'])
 
-def es_bulk_index(bodys):
-    bodys = list(filter(lambda x: not  _es_chk_exist(x['title']), bodys))
+def es_bulk_index(bodys, _index='test'):
+    bodys = list(filter(lambda x: not  _es_chk_exist(_index, ['title']), bodys))
     print('add:' +str(len(bodys)))
     start_time = time.time()
     bodys = list(map(lambda x: dict(x ,**{"timestamp": datetime.datetime.utcnow()}), bodys))
-    actions = list(map(lambda x: {"timestamp": datetime.datetime.utcnow(), "_index": "rss_by_crawler","_source": x}, bodys))
+    actions = list(map(lambda x: {"timestamp": datetime.datetime.utcnow(), "_index": _index,"_source": x}, bodys))
     res = helpers.bulk(es, actions)
     end_time = time.time()
     print("{} {}s".format(res, end_time - start_time))
@@ -34,8 +34,7 @@ def es_bulk_index(bodys):
 def es_index_search(_index,kword):
     import json
     #res = es.search(index="test", body={"query": {"match_all": {}}})
-    #res = es.search(index="test", q="author : *")
-    
+        
     # 查询语句需要修改一下，很傻
     # res = es.search(index="test", q="author : {kword} or title : {kword} or description : {kword}".format(kword=kword))
         
@@ -48,7 +47,7 @@ def es_index_search(_index,kword):
     for hit in res['hits']['hits']:
         try:
             #print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
-            print(dict(hit["_source"], **{"_score" :hit["_score"]}))
+            # print(dict(hit["_source"], **{"_score" :hit["_score"]}))
             res_list.append(dict(hit["_source"], **{"_score" :hit["_score"]}))
         except KeyError as e:
             print(repr(e))
