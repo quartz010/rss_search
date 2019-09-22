4d1a882adf24c2d19e8d868dc122c9eefe2b4235
ramonesliu <ramonesliu@tencent.com>
2019-09-22 18:38:03 +0800

    秘制bug，装饰器没有返回值。

--- web/models/elastic.py
+++ web/models/elastic.py
@@ -7,6 +7,18 @@ from elasticsearch import helpers
 ES_HOST = 'sgk:9200'
 es = elasticsearch.Elasticsearch([ES_HOST])
 
+def time_me(fn):
+    u'''
+    函数耗时修饰器
+    '''
+    def _wrapper(*args, **kwargs):
+        start = time.clock()
+        ret = fn(*args, **kwargs)
+        print(u"%s cost %s second"%(fn.__name__, time.clock() - start))
+        return ret
+    return _wrapper
+
+
 def _es_chk_exist(_index, _title):
     """
         用来判断是否条目已存在的函数，这里会是一个性能瓶颈，需要进行单条的遍历
@@ -22,9 +34,10 @@ def es_index(_bodys, _index='test'):
         rst = es.index(index=_index, body=body, id=None)
         print(rst['result'])
 
+@time_me
 def _es_chk_id_exist(_index, _id):
     """
-        用来判断是否条目已存在的函数，现在直接使用 HEAD
+        用来判断是否条目已存在的函数，现在直接使用 HEAD 比前面的轮询好一点
     """
     import requests
     res = requests.head("http://"+ES_HOST+'/_doc/'+_id)
@@ -36,18 +49,17 @@ def gen_es_id(org_str):
     hash_str = hashlib.md5(org_str.encode("utf-8")).hexdigest()
     return hash_str
 
+@time_me
 def es_bulk_index(_bodys, _index='test'):
-
+    # 新函数使用ID作为唯一索引
     bodys = list(filter(lambda x: not _es_chk_id_exist(_index, gen_es_id(x['title'])), _bodys))
     print('add:' +str(len(bodys)))
-    start_time = time.time()
     # 给每一项补充时间戳
     bodys = list(map(lambda x: dict(x ,**{"timestamp": datetime.datetime.utcnow()}), bodys))
     # 使用自定义的MD5ID
     actions = list(map(lambda x: {"_index": _index,"_id": gen_es_id(x['title']) ,"_source": x}, bodys))
     res = helpers.bulk(es, actions)
-    end_time = time.time()
-    print("{} {}s".format(res, end_time - start_time))
+
 
 def es_index_search(_index,kword):
     import json
